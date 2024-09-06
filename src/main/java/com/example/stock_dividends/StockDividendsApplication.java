6e84d6e6fb6fa71bb8e36f391d5bba7fd370a32e
ParkJunYoung <computer9302@gmail.com>
2024-09-06 23:00:08 +0900

    first commit! base code

---
+++ src/main/java/com/example/stock_dividends/StockDividendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_dividends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDividendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDividendsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stock_dividends/StockDividendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_dividends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDividendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
