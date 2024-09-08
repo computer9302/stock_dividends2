e006ed85892a93dc295b53a67414706ea1a6de6f
ParkJunYoung <computer9302@gmail.com>
2024-09-08 23:06:52 +0900

    Chapter05. 회원 관리 - 05_회사 삭제 기능 구현

--- src/main/java/com/dayone/web/AuthController.java
+++ src/main/java/com/dayone/web/AuthController.java
@@ -1,5 +1,9 @@
 package com.dayone.web;
 
+import com.dayone.model.Auth;
+import com.dayone.security.TokenProvider;
+import com.dayone.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,17 +11,25 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(){
+    private final MemberService memberService;
 
-        return null;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request){
+        // 회원가입을 위한 API
+        var result = this.memberService.register(request);
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> signin(){
-
-        return null;
+    public ResponseEntity<?> signin(@RequestBody Auth.SignIn request){
+        // 로그인용 API
+        var member = this.memberService.authenticate(request);
+        var token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+        return ResponseEntity.ok(token);
     }
 }
